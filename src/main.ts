c0e41d1ae1a823ddc081f999e3ba78c797d514b4
Weilence <weilence@163.com>
2024-05-25 16:37:02 +0800

    chore: add tailwindcss and naive-ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import "./index.css";
 
-createApp(App).mount("#app");
+const meta = document.createElement("meta");
+meta.name = "naive-ui-style";
+document.head.appendChild(meta);
+
+import { createApp } from "vue";
+import naive from "naive-ui";
+
+import App from "./App.vue";
+createApp(App).use(naive).mount("#app");
